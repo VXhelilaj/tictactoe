0c0168792c76f90b91295757b6c08bc7ce83e3ec
viktor <v.xhelilaj@gmail.com>
2016-09-14 15:58:57 -0400

    used magic box method to determine winner

--- ttt.js
+++ ttt.js
@@ -1,26 +1,99 @@
-'use strict'
+// 'use strict'
+//
+// // Ensure the DOM is ready first!
+//
+// $(function() {
+//   var turn = 0;
+//
+//   $('.square').on('click', function() {
+//     // alert("A square was clicked");
+//     var self = $(this);
+//
+//     self.data('position');
+//
+//     if ( self.text() === '' ) {
+//       if ( turn % 2 === 0 ) {
+//         self.text('X');
+//       } else {
+//         self.text('O');
+//       }
+//
+//       turn++;
+//     } else {
+//       alert("Damn bruh...");
+//     }
+//   });
+// });
 
-// Ensure the DOM is ready first!
 
-$(function() {
-  var turn = 0;
+$(function () {
 
-  $('.square').on('click', function() {
-    // alert("A square was clicked");
-    var self = $(this);
+    var squares = [],
+        SIZE = 3,
+        EMPTY = "&nbsp;",
+        score,
+        moves,
+        turn = "X",
 
-    self.data('position');
+wins = [7, 56, 448, 73, 146, 292, 273, 84],
 
-    if ( self.text() === '' ) {
-      if ( turn % 2 === 0 ) {
-        self.text('X');
-      } else {
-        self.text('O');
+startNewGame = function () {
+     turn = "X";
+     score = {"X": 0, "O": 0};
+     moves = 0;
+     squares.forEach(function (square) {square.html(EMPTY);});
+ },
+
+
+ win = function (score) {
+        for (var i = 0; i < wins.length; i += 1) {
+            if ((wins[i] & score) === wins[i]) {
+                return true;
+            }
+        }
+        return false;
+    },
+
+    set = function () {
+
+       if ($(this).html() !== EMPTY) {
+           return;
+       }
+       $(this).html(turn);
+       console.log($(this));
+       moves += 1;
+       score[turn] += $(this)[0].indicator;
+       console.log(score[turn]);
+       if (win(score[turn])) {
+           alert(turn + " wins!");
+           startNewGame();
+       } else if (moves === SIZE * SIZE) {
+           alert("Cat\u2019s game!");
+           startNewGame();
+       } else {
+           turn = turn === "X" ? "O" : "X";
+       }
+   },
+
+   play = function () {
+      var board = $("<table border=1 cellspacing=0>"), indicator = 1;
+      for (var i = 0; i < SIZE; i += 1) {
+          var row = $("<tr>");
+          board.append(row);
+          for (var j = 0; j < SIZE; j += 1) {
+              var cell = $("<td height=50 width=50 align=center valign=center></td>");
+              cell[0].indicator = indicator;
+              cell.click(set);
+              row.append(cell);
+              squares.push(cell);
+              indicator += indicator;
+          }
       }
 
-      turn++;
-    } else {
-      alert("Damn bruh...");
-    }
-  });
+      // Attach under tictactoe if present, otherwise to body.
+      $(document.getElementById("tictactoe") || document.body).append(board);
+      startNewGame();
+  };
+
+  play();
 });
